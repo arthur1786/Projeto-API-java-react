595e1c6bbe13ce6ac55593f17e7af65645ac941f
arthur1786 <arthurhp2003@gmail.com>
2022-09-07 19:14:18 -0300

    Project clean

---
+++ dsmeta/backend/src/main/java/com/devsuperior/ProjetoReact/ProjetoReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReactApplication.class, args);
+	}
+
+}
---
+++ dsmeta/backend/src/test/java/com/devsuperior/ProjetoReact/ProjetoReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
